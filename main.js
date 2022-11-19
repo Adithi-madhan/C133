081b8d58f4779e18e1ea1e00aaeb16eca4f369b9
Adithi-madhan <90496212+Adithi-madhan@users.noreply.github.com>
2022-11-19 20:41:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+img = "";
+status = "";
+objects = [];
+object_detector = "";
+
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(600,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Dedecting Objects";
+}
+
+function draw()
+{
+    image(img,0,0,640,420);
+
+      if(status != "")
+      {
+        for (i = 0; i < objects.length; i++)
+    {
+        document.getElementById("status").innerHTML = "Status : Object Detected";
+
+        fill('#FF000');
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "%" , objects[i].x + 15,object[i].y + 15);
+        noFill();
+        stroke('FF000');
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+      }
+    }
+    fill('#FF000');
+    text("Dog", 45 , 75)
+    noFill();
+    stroke('#FF000');
+    rect(30,60,450,350);
+
+    fill('#FF000');
+    text("Cat", 320 ,120)
+    noFill();
+    stroke('#FF000');
+    rect(300,90,270,320);
+}
+
+function modelLoaded() {
+    console.log("Model Loaded! ")
+    status = true;
+    objectDedector.detect(img, gotResult);
+
+}
+
+function gotResult(error , results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
